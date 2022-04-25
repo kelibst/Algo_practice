89be6f7cf4258d42a73b360ba804b6709b6b0c7b
Keli Booster <kbooster17@gmail.com>
2022-04-25 19:35:17 +0000

    add dataFinder and stepCounter

---
+++ hackerRank_arrays/JavaScript/dataFinder.js
@@ -0,0 +1,27 @@
+function dataFinder(data) {
+  // Write your code here
+
+  const find = (minRange, maxRange, value) => {
+    try {
+      const res = data.includes(value, minRange);
+      return res;
+    } catch (e) {
+      return "Invalid range";
+    }
+  };
+  return find;
+}
+
+
+function dataFinder(data) {
+    // Write your code here
+    
+   const find = (minRange, maxRange, value) => {
+        try {
+            if (maxRange >= data.length) {
+                throw new Error("Error: Invalid range")
+…        return "Error: Invalid range"
+    }
+   }
+   return find
+}
