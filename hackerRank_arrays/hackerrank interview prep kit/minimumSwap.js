781be7b448727520dbe3dd920032d6701182d06c
Keli Booster <kbooster17@gmail.com>
2022-09-06 22:11:34 +0000

    update minimum swap

--- hackerRank_arrays/hackerrank interview prep kit/minimumSwap.js
+++ hackerRank_arrays/hackerrank interview prep kit/minimumSwap.js
@@ -4,30 +4,16 @@ function minimumSwaps(arr) {
   let currentVal = 0;
   let originalInd = 0;
   let alienVal = 0;
-  while (x < arr.length) {
-    currentVal = arr[x];
-    originalInd = currentVal - 1;
-    alienVal = arr[originalInd];
-    // console.log(currentVal)
-    if (currentVal !== x + 1) {
+  for (let x = 0; x < arr.length; x++) {
+    while (arr[x] !== x + 1) {
+      //while the current element value is not at the right position, keep swaping position until it does
+      currentVal = arr[x];
+      originalInd = currentVal - 1;
+      alienVal = arr[originalInd];
       arr[x] = alienVal;
       arr[originalInd] = currentVal;
-      console.log("arr", arr);
       count += 1;
-
-      if (arr[x] !== x + 1) {
-        currentVal = arr[x];
-        originalInd = currentVal - 1;
-        alienVal = arr[originalInd];
-        arr[x] = alienVal;
-        arr[originalInd] = currentVal;
-        console.log("arr", arr);
-        count += 1;
-      }
     }
-    x += 1;
   }
-
-  console.log(count);
   return count;
 }
