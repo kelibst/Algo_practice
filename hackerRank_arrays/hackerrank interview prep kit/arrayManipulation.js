b33402691f60995c52dbf8b81b77a9136da80fab
Keli Booster <kbooster17@gmail.com>
2022-09-09 09:21:41 +0000

    solve all array manipulation problems

--- hackerRank_arrays/hackerrank interview prep kit/arrayManipulation.js
+++ hackerRank_arrays/hackerrank interview prep kit/arrayManipulation.js
@@ -6,10 +6,22 @@ function arrayManipulation(n, queries) {
     let endInd = val[1];
     let k = val[2];
     resultArr[startInd - 1] += k;
-    if (endInd + 1 < n) {
+    if (endInd < n) {
       resultArr[endInd] -= k;
     }
   });
   console.log(resultArr);
-  return resultArr.reduce((accumulator, curValue) => accumulator + curValue, 0);
+  let maxNum = 0;
+  resultArr.forEach((val, ind) => {
+    if (ind !== 0) {
+      resultArr[ind] = val += resultArr[ind - 1];
+      if (resultArr[ind] > maxNum) {
+        maxNum = resultArr[ind];
+      }
+    }
+  });
+
+  return maxNum;
 }
+// link
+// https://www.hackerrank.com/challenges/crush/problem?h_l=interview&isFullScreen=true&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays
