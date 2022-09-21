f582a61a4221ac305e8327fccfcd99005a8af62c
Keli Booster <kbooster17@gmail.com>
2022-09-21 21:05:41 +0000

    update frequencyQueries

--- hackerRank_arrays/hackerrank interview prep kit/frenquenciQueries.js
+++ hackerRank_arrays/hackerrank interview prep kit/frenquenciQueries.js
@@ -1,5 +1,34 @@
 // Complete the freqQuery function below.
 function freqQuery(queries) {
-  console.log(queries);
-  return [0, 0];
+  let data = {};
+  let result = [];
+  let resArr = [];
+  for (let x = 0; x < queries.length; x++) {
+    let command = queries[x][0];
+    let val = queries[x][1];
+    if (command === 1 && data[val]) {
+      data[val] = data[val] += 1;
+      result[val] = result[val] += 1;
+    } else if (command === 1) {
+      data[val] = 1;
+      result[val] = 1;
+    }
+
+    if (command === 2 && data[val]) {
+      data[val] = data[val] -= 1;
+      result[val] = result[val] -= 1;
+    }
+
+    if (command === 3) {
+      // console.log(Object.keys(data))
+      if (result.includes(val)) {
+        resArr.push(1);
+      } else {
+        resArr.push(0);
+      }
+    }
+  }
+
+  console.log(resArr);
+  return resArr;
 }
