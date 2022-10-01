8a794bc8c34328cf3203566b1e2637747edafb21
Keli Booster <kbooster17@gmail.com>
2022-10-01 17:18:17 +0000

    add stack base on linkedlist

---
+++ hackerRank_arrays/dist/abstract-data-type/LinkListStack.ts
@@ -0,0 +1,47 @@
+const LinkedLists = require("./LinkListWithTail");
+
+class LinkedListStack {
+  list;
+  constructor() {
+    this.list = new LinkedLists();
+  }
+
+  push(value) {
+    this.list.prepend(value);
+  }
+
+  pop() {
+    return this.list.rmFromEnd();
+  }
+
+  peek() {
+    return this.list.head.value;
+  }
+
+  isEmpty() {
+    return this.list.isEmpty();
+  }
+
+  getSize() {
+    return this.list.getSize();
+  }
+
+  print() {
+    return this.list.print();
+  }
+}
+
+const stack = new LinkedListStack();
+
+console.log(stack.isEmpty());
+stack.push(20);
+stack.push(30);
+stack.push(40);
+stack.push(50);
+stack.push(60);
+
+console.log(stack.getSize(), "size");
+stack.print();
+console.log(stack.peek(), "peek");
+console.log(stack.pop());
+stack.print();
--- hackerRank_arrays/dist/abstract-data-type/LinkListWithTail.js
+++ hackerRank_arrays/dist/abstract-data-type/LinkListWithTail.js
@@ -1,89 +1,85 @@
 class Link {
-    constructor(value) {
-        this.value = value;
-        this.next = this.next;
-    }
+  constructor(value) {
+    this.value = value;
+    this.next = this.next;
+  }
 }
 class linkedlist {
-    constructor() {
-        this.head = null;
-        this.tail = null;
-        this.size = 0;
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+  }
+  isEmpty() {
+    return this.size === 0;
+  }
+  getSize() {
+    return this.size;
+  }
+  print() {
+    if (this.isEmpty()) {
+      console.log("List is empty");
+    } else {
+      let curr = this.head;
+      let listVals = "";
+      while (curr) {
+        listVals += `${curr.value} `;
+        curr = curr.next;
+      }
+      console.log(listVals);
     }
-    isEmpty() {
-        return this.size === 0;
+  }
+  prepend(value) {
+    const node = new Link(value);
+    if (this.isEmpty()) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      node.next = this.head;
+      this.head = node;
     }
-    getSize() {
-        return this.size;
+    this.size++;
+  }
+  append(value) {
+    const node = new Link(value);
+    if (this.isEmpty()) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
     }
-    print() {
-        if (this.isEmpty()) {
-            console.log("List is empty");
-        }
-        else {
-            let curr = this.head;
-            let listVals = "";
-            while (curr) {
-                listVals += `${curr.value} `;
-                curr = curr.next;
-            }
-            console.log(listVals);
-        }
+    this.size++;
+  }
+  rmFromFront() {
+    if (this.isEmpty()) {
+      console.log("the list is empty");
+      return null;
     }
-    prepend(value) {
-        const node = new Link(value);
-        if (this.isEmpty()) {
-            this.head = node;
-            this.tail = node;
-        }
-        else {
-            node.next = this.head;
-            this.head = node;
-        }
-        this.size++;
+    const value = this.head.value;
+    this.head = this.head.next;
+    this.size++;
+    return value;
+  }
+  rmFromEnd() {
+    if (this.isEmpty()) {
+      return null;
     }
-    append(value) {
-        const node = new Link(value);
-        if (this.isEmpty()) {
-            this.head = node;
-            this.tail = node;
-        }
-        else {
-            this.tail.next = node;
-            this.tail = node;
-        }
-        this.size++;
+    const value = this.tail.value;
+    if (this.size === 1) {
+      this.tail = null;
+      this.head = null;
+    } else {
+      let prev = this.head;
+      while (prev.next !== this.tail) {
+        prev = prev.next;
+      }
+      prev.next = null;
+      this.tail = prev;
     }
-    rmFromFront() {
-        if (this.isEmpty()) {
-            console.log("the list is empty");
-            return null;
-        }
-        const value = this.head.value;
-        this.head = this.head.next;
-        this.size++;
-        return value;
-    }
-    rmFromEnd() {
-        if (this.isEmpty()) {
-            return null;
-        }
-        const value = this.tail.value;
-        if (this.size === 1) {
-            this.tail = null;
-            this.head = null;
-        }
-        else {
-            let prev = this.head;
-            while (prev.next !== this.tail) {
-                prev = prev.next;
-            }
-            prev.next = null;
-            this.tail = prev;
-        }
-        this.size--;
-        return value;
-    }
+    this.size--;
+    return value;
+  }
 }
 const List = new linkedlist();
 console.log("list is empty?", List.isEmpty());
