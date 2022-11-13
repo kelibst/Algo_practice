476e8e1852d09270507a6d3ffdceb50626723f14
Keli Booster <kbooster17@gmail.com>
2022-11-13 20:36:03 +0000

    add lessons for callback

--- hackerRank_arrays/JavaScript/hoursPassed.js
+++ hackerRank_arrays/JavaScript/hoursPassed.js
@@ -2,29 +2,63 @@ function hoursPassed(t1, t2) {
   const regex = /([^:]+)/;
   let l1Num = Number(t1.match(regex)[0]);
   let l2Num = Number(t2.match(regex)[0]);
-
+  let res = 0;
   if (
     (t1.includes("AM") && t2.includes("AM")) ||
     (t1.includes("PM") && t2.includes("PM"))
   ) {
-    if (l2Num > l1Num) {
-      let res = l2Num - l1Num;
-      return `${res} hours`;
+    if (l2Num - l1Num > 0) {
+      res = l2Num - l1Num;
+    } else if (Math.abs(l2Num - l1Num)) {
+      res = 24 - Math.abs(l2Num - l1Num);
     }
-    let res = l2Num - l1Num + 12;
-    return `${res} hours`;
-  } else if (t1.includes("AM") && t2.includes("PM")) {
-    let res = 12 - l1Num + l2Num;
-    return `${res} hours`;
-  } else if (t1.includes("PM") && t2.includes("AM")) {
-    let res = 12 - l1Num + (12 - l2Num);
-    return `${res} hours`;
+  } else if (
+    (t1.includes("AM") && t2.includes("PM")) ||
+    (t1.includes("PM") && t2.includes("AM"))
+  ) {
+    res = 12 - l1Num + l2Num;
   }
-  return "No time has passed.";
+  return res ? `${res} hours` : "No time has passed.";
 }
+
+console.log(hoursPassed("3:00 AM", "9:00 AM"));
+console.log(hoursPassed("3:00 AM", "1:00 AM"));
+console.log(hoursPassed("2:00 PM", "4:00 PM"));
+
+console.log(hoursPassed("1:00 AM", "3:00 PM"));
+console.log(hoursPassed("3:00 PM", "3:00 AM"));
+
+console.log(hoursPassed("12:00 AM", "10:00 PM"));
+
+// let [strVector, resVector] = [
+//   [
+//     ["3:00 AM", "9:00 AM"],
+//     ["2:00 PM", "4:00 PM"],
+//     ["1:00 AM", "3:00 PM"],
+//     ["2:00 AM", "3:00 PM"],
+//     ["8:00 AM", "8:00 PM"],
+//     ["12:00 AM", "10:00 PM"],
+//     ["1:00 AM", "1:00 AM"],
+//     ["12:00 PM", "12:00 PM"],
+//   ],
+//   [
+//     "6 hours",
+//     "2 hours",
+//     "14 hours",
+//     "13 hours",
+//     "12 hours",
+//     "22 hours",
+//     "No time has passed.",
+//     "No time has passed.",
+//   ],
+// ];
+// for (let i in strVector)
+//   Test.assertSimilar(hoursPassed(...strVector[i]), resVector[i]);
 
 // both is am or pm
 // 8am 10am 1pm 5pm
+
+// t1 = 4am t2 = 1am
 
 //1 am another pm
 //11 am 10pm
