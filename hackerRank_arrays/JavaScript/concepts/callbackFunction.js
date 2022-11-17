15dd0c1f3e5686c7ab94e4e36d6f23fb36ae8603
Keli Booster <kbooster17@gmail.com>
2022-11-17 22:01:19 +0000

    add iterators advance concepts

--- hackerRank_arrays/JavaScript/concepts/callbackFunction.js
+++ hackerRank_arrays/JavaScript/concepts/callbackFunction.js
@@ -1,31 +1,52 @@
 // A callback function is a function passed into another function as an argument, which is then invoked inside the outer function to complete some kind of routine or action.
 // allbacks are often used to continue code execution after an asynchronous operation has completed — these are called asynchronous callbacks.
+const arr = [1, 2, 3];
+const print = (val, func) => {
+  return setTimeout(() => {
+    console.log(val);
+    func();
+  }, 4000);
+};
 
-const printString = (string) => {
+const printAll = () => {
+  print(arr[0], () => {
+    print(arr[2], () => {
+      print(arr[1], () => {
+        console.log("done");
+      });
+    });
+  });
+};
+// printAll();
+
+const printPromise = (val) => {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      console.log(string);
+      console.log(val);
       resolve();
-    }, 1000);
+    }, 3000);
   });
 };
 
-function printAll() {
-  printString("A")
-    .then(() => {
-      return printString("B");
-    })
-    .then(() => {
-      return printString("C");
+const printAllPromise = () => {
+  return printPromise(arr[0]).then(() => {
+    return printPromise(arr[2]).then(() => {
+      return printPromise(arr[1]);
     });
-}
-
-printAll();
-
-const printAlls = (arr) => {
-  arr.forEach(async (ar) => {
-    await printString(ar);
   });
 };
 
-printAlls(["D", "E", "F"]);
+printAllPromise();
+
+var nums = [1, 5, 16];
+
+for (
+  // initialisation
+  var iter = nums[Symbol.iterator](), next = iter.next(), num = next.value;
+  // iterative check
+  !next.done;
+  // iterative expressions
+  next = iter.next(), num = next.value
+) {
+  console.log(num);
+}
